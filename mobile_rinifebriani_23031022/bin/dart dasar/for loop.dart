51300ac0dddb75ddedab27163625f772370aa5f6
rinifebriani765-igtm <rinifebriani765@gmail.com>
2025-10-10 14:01:29 +0800

    for loop

---
+++ mobile_rinifebriani_23031022/bin/dart dasar/for loop.dart
@@ -0,0 +1,17 @@
+void main() {
+  for (;;) {
+    print('perulangan tanpa henti');
+  }
+
+  // perulangan dengan kondisi
+  var counter = 1;
+
+  for (; counter <= 10;) {
+    print('perulanganke-$counter');
+    counter++;
+  }
+
+  // perulangan dengan statement
+  // for (var counter =1; counter <=; counter++){}
+  print('perulangan ke-$counter');
+}
