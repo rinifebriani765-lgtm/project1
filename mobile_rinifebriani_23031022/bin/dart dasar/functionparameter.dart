f471b03cf29a84c5d62059847ee4851dd6b39f36
rinifebriani765-igtm <rinifebriani765@gmail.com>
2025-10-10 14:10:17 +0800

    function parameter

---
+++ mobile_rinifebriani_23031022/bin/dart dasar/functionparameter.dart
@@ -0,0 +1,8 @@
+void sayHello(String firstName, String lastname) {
+  print('hello $firstName $lastname');
+}
+
+void main() {
+  sayHello('Rini', 'Febriani');
+  sayHello('Stmik', 'Banjamasin');
+}
