e7f574457315ed9262f40b928b40b5567bf7744f
rinifebriani765-igtm <rinifebriani765@gmail.com>
2025-10-17 21:08:07 +0800

    Kode Named Parameter

---
+++ mobile_rinifebriani_23031022/bin/dart dasar/kodeNamedparameter.dart
@@ -0,0 +1,11 @@
+void sayHello({String? firstname, String? lastname}) {
+  print('hello $firstname $lastname');
+}
+
+void main() {
+  sayHello(firstname: 'Rini', lastname: 'Febriani');
+  sayHello(lastname: 'noys', firstname: 'rin');
+  sayHello();
+  sayHello(firstname: 'Rini');
+  sayHello(lastname: 'noys');
+}
