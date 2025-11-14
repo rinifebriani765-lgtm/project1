e4d8c2a484d459e356376f2e95f362d1adf982d3
rinifebriani765-igtm <rinifebriani765@gmail.com>
2025-11-14 14:13:53 +0800

    static field

---
+++ mobile_rinifebriani_23031022/bin/dart dasar/OOP/static field.dart
@@ -0,0 +1,9 @@
+class Application {
+  static final String author = "Rini febriani";
+  static final String name = "Belajar Pemrograman Dart";
+}
+
+void main() {
+  print(Application.name);
+  print(Application.author);
+}
