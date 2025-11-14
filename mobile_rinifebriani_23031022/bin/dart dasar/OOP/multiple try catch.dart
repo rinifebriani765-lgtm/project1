ee9112f4c23c4fa2168ccf5e51da087acdb70b5a
rinifebriani765-igtm <rinifebriani765@gmail.com>
2025-11-14 14:35:15 +0800

    multiple try catch

---
+++ mobile_rinifebriani_23031022/bin/dart dasar/OOP/multiple try catch.dart
@@ -0,0 +1,29 @@
+class ValidationException implements Exception {
+  String message;
+  ValidationException(this.message);
+
+  @override
+  String toString() => "ValidationException: $message";
+}
+
+class Validation {
+  static void validate(String username, String password) {
+    if (username == "") {
+      throw ValidationException("Username is blank");
+    } else if (password == "") {
+      throw ValidationException("Password is blank");
+    } else if (username != "Rini" || password != "Rini") {
+      throw Exception("Login failed");
+    }
+  }
+}
+
+void main() {
+  try {
+    Validation.validate("Rini", "salah");
+  } on ValidationException catch (exception) {
+    print("Error: ${exception.message}");
+  } on Exception catch (exception) {
+    print("Error: ${exception.toString()}");
+  }
+}
