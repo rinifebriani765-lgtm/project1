5f865e603f7ec725b4806c7500a4dfc50e7c1047
rinifebriani765-igtm <rinifebriani765@gmail.com>
2025-11-14 15:19:41 +0800

    mengakses method overidding

---
+++ mobile_rinifebriani_23031022/bin/dart dasar/OOP/mengakses method overidding.dart
@@ -0,0 +1,24 @@
+class Manager {
+  String? name;
+
+  void sayHello(String name) {
+    print('Hello $name, my name is Manager ${this.name}');
+  }
+}
+
+class VicePresident extends Manager {
+  @override
+  void sayHello(String name) {
+    print('Hello $name, my name is VP ${this.name}');
+  }
+}
+
+void main() {
+  var manager = Manager();
+  manager.name = "Rini";
+  manager.sayHello('lili');
+
+  var vp = VicePresident();
+  vp.name = "Febriani";
+  vp.sayHello('lili');
+}
